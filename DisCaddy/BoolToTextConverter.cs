b0aa1fe5d201d273c3ca8f468f9e4f05dbf71f40
Ethan Reed <reed2ep@mail.uc.edu>
2025-07-24 22:36:12 -0400

    Added ability to toggle add disc command

---
+++ DisCaddy/BoolToTextConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisCaddy
+{
+    public class BoolToTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string[] texts = ((string)parameter).Split('|');
+            return (bool)value ? texts[1] : texts[0];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
